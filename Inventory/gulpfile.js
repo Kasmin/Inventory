f76995977501bb65b7f8706e9820164f58d03d6a
Kasmin <victor.kasmin@gmail.com>
2019-02-09 00:30:30 +0300

    Remove BootsratpCss, add Gulp

---
+++ Inventory/gulpfile.js
@@ -0,0 +1,42 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    less = require("gulp-less"),
+    sass = require("gulp-sass"); // добавляем модуль sass
+
+var browserSync = require('browser-sync').create();
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+// регистрируем задачу для конвертации файла scss в css
+gulp.task("sass", function (done) {
+    gulp.src('src/scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(paths.webroot + '/css'))
+        .pipe(browserSync.stream());
+
+    done();
+});
+
+gulp.task('serve', function (done) {
+
+    browserSync.init({
+        https: true,
+        proxy: "https://localhost:44361",
+        //files: ["src/scss/*.scss", "Views/**/*.cshtml"]
+    });
+
+    gulp.watch("src/scss/*.scss", gulp.series('sass'));
+    gulp.watch("Views/**/*.cshtml").on('change', () => {
+        browserSync.reload();
+        done();
+    });
+
+
+    done();
+});
+
+gulp.task('default', gulp.series('sass', 'serve'));
